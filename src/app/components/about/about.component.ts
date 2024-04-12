9d60cf9b79d1c87faf30c04dc03c4e3549e692c2
Amil Abdullazadeh <amil.abdullazadeh@vabiss.com>
2024-04-12 22:53:44 +0400

    xox game

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
+    todo: string = '';
+    todos: string[] = [];
 
+    add() {
+        this.todos.push(this.todo);
+        this.todo = '';
+    }
+
+    remove(index: number) {
+      if (confirm('Are you sure you want to delete this item?'))
+        this.todos.splice(index, 1);
+    }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,10 +7,43 @@ import {Router} from "@angular/router";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  mark: string = ''
+  marks: string[] = []
+  moves: any[] = []
+
+  newGame() {
+    this.marks = [
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+    ]
+    this.mark = 'X'
+    this.moves = []
+  }
+
+  add(index: number) {
+    if (this.marks[index] === '') {
+      this.marks[index] = this.mark
+      this.moves.push([...this.marks])
+      this.mark = this.mark === 'X' ? 'O' : 'X'
+    }
+  }
+
+  returnMove(index: number) {
+    this.marks = this.moves[index]
+  }
 
   constructor(
     private router: Router
-  ) {}
+  ) {
+    this.newGame()
+  }
 
   gotoHome() {
     this.router.navigate(['/home']);
